208ba9e9224484edc3d40662fb7966e2056f3edd
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-05-28 20:35:41 +0300

    rewrote second task with easier implementation, with just counters

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ public class main {
     
     public static void main(String[] args){
         checkLetters("", "f");
-        System.out.println(FruitBasket.collectFruits(new int[]{1,2,1}));
+        System.out.println(FruitBasket.collectFruits(new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4}));
     }
 
     public static void checkLetters(String original, String added){
--- src/main/java/tasks/FruitBasket.java
+++ src/main/java/tasks/FruitBasket.java
@@ -5,41 +5,38 @@ import java.util.*;
 public class FruitBasket {
 
     public static int collectFruits(int[] trees){
-        List<Integer> bigSelection = new ArrayList<>();
-        Set<Integer> currentFruits = new HashSet<>();
-        int biggestSize = 0;
-
-        for (int fruit :
-                trees) {
-            if(currentFruits.contains(fruit)){
-                bigSelection.add(fruit);
+        //basket 1 holds the latest type of fruit, basket 0 holds the one before that
+        //-1 means an empty basket
+        int[] baskets = {-1, -1};
+        int maxFruits = 0;
+        int fruitsNow = 0;
+        int lastConsecFruits = 0;
+        for(int fruit : trees){
+            if(baskets[1] == fruit ){
+                fruitsNow++;
+                lastConsecFruits++;
             }else{
-                if(currentFruits.size() != 2){
-                    currentFruits.add(fruit);
-                    bigSelection.add(fruit);
-                }else{
-                    if(biggestSize < bigSelection.size()) { biggestSize = bigSelection.size(); }
-                    List<Integer> tempFruits = new ArrayList<>(bigSelection);
-                    Collections.reverse(tempFruits);
-                    bigSelection.clear();
-                    int lastFruit = tempFruits.get(0);
-                    currentFruits.clear();
-                    currentFruits.add(lastFruit);
-                    currentFruits.add(fruit);
-                    for (int oldFruit :
-                            tempFruits) {
-                        if (oldFruit == lastFruit) {
-                            bigSelection.add(oldFruit);
-                            bigSelection.add(fruit);
-                        }else{
-                            break;
-                        }
+                //if our old basket is not empty, only leave the fruits we picked the last
+                //if this is the max amount of fruits, record it
+                if(baskets[0] != -1 && baskets[0] != fruit){
+                    if(fruitsNow > maxFruits){
+                        maxFruits = fruitsNow;
                     }
+                    fruitsNow = lastConsecFruits;
                 }
+                //latest fruit type becomes old, new tree fruit type becomes latest
+                baskets[0] = baskets[1];
+                baskets[1] = fruit;
+                fruitsNow++;
+                lastConsecFruits = 1;
             }
         }
-        if(biggestSize < bigSelection.size()) { biggestSize = bigSelection.size(); }
-        return biggestSize;
+        if(fruitsNow > maxFruits){
+            maxFruits = fruitsNow;
+        }
+
+
+        return maxFruits;
     }
 
 }
